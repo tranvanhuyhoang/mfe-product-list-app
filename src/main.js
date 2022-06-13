19db3a60cea501374d725af08e2618946eb43a67
hoangtvh <tranvanhuyhoang@gmail.com>
2022-06-13 17:46:24 +0700

    basic setup

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import "./set-public-path";
+import { h, createApp } from "vue";
+import singleSpaVue from "single-spa-vue";
+// import router from "./router";
+import App from "./App.vue";
+
+const vueLifecycles = singleSpaVue({
+  createApp,
+  appOptions: {
+    render() {
+      return h(App, {
+        name: this.name,
+        mountParcel: this.mountParcel,
+        singleSpa: this.singleSpa,
+      });
+    },
+  },
+  //   handleInstance: (app) => {
+  //     app.use(router);
+  //   },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
